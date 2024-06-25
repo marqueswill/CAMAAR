822392b050a9020b60136a0507ee8d8ae3ec8b7f
jpantonow <jpantonow@gmail.com>
2024-06-24 21:47:04 -0300

    increasing score rubycritic

--- camaar/app/controllers/admins_controller.rb
+++ camaar/app/controllers/admins_controller.rb
@@ -184,36 +184,7 @@ class AdminsController < ApplicationController
 
   def generate_summary
     resumo = {}
-
-    @form_questions.each do |question|
-      answers = if @form.role == 'discente'
-                  StudentAnswer.where(form_question_id: question.id)
-                else
-                  TeacherAnswer.where(form_question_id: question.id)
-                end
-
-      question_body = JSON.parse(question.body)
-      answers.each do |answ|
-        answer_body = JSON.parse(answ.answers)['answers']
-
-        case question.question_type
-        when 'text'
-          resumo[question.title] ||= []
-          resumo[question.title] << answer_body
-        when 'multiple_choice'
-          resumo[question.title] ||= {}
-          question_body['options'].each do |option|
-            resumo[question.title][option[1]] ||= 0 if option[1] != ''
-          end
-
-          answer_body.each do |k, selected|
-            resumo[question.title][question_body['options'][k]] += 1 if selected
-          end
-        end
-      end
-    end
-
-    resumo
+    SummaryService.call(resumo, @form_questions, @form)
   end
 
   def export_to_csv
@@ -246,9 +217,10 @@ class AdminsController < ApplicationController
   end
 
   def generate_graph
-    if @form.role == 'discente'
+    case @form.role
+    when 'discente'
       ch = ExportPngService.generate_teacher_graph(@form)
-    elsif @form.role == 'docente'
+    when 'docente'
       ch = ExportPngService.generate_student_graph(@form)
     end
     ch
