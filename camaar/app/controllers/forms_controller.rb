09d337f4c5fd9e5f046a7a282c34b3c3aa25f12b
Marcos <marcosp.oliveir4@gmail.com>
2024-06-25 22:36:16 -0300

    refatorando

--- camaar/app/controllers/forms_controller.rb
+++ camaar/app/controllers/forms_controller.rb
@@ -1,63 +1,70 @@
 class FormsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user_data
+  before_action :initializer
   layout "user"
 
-  def index
+  attr_reader :enrollments, :filter, :errors, :pending_forms, :answered_forms, :questions_and_answers, :subject_classes
+
+  def initializer
+    @enrollments = Enrollment.where(student_id: @student.id)
     @filter = params[:filter] || "pending"
     @errors = []
+    @pending_forms = []
+    @answered_forms = []
+    @questions_and_answers = []
+    @subject_classes = nil
+  end
+
+  def index
 
     occupation = current_user.occupation
     case occupation
     when "discente"
-      @enrollments = Enrollment.where(student_id: @student.id)
-      if @enrollments.empty?
-        @errors << "Parece que você não está matriculado em nenhum disciplina."
+      if enrollments.empty?
+        errors << "Parece que você não está matriculado em nenhum disciplina."
       else
-        @subject_classes = SubjectClass.where({ id: @enrollments.pluck(:subject_class_id) })
+        subject_classes = SubjectClass.where(id: enrollments.pluck(:subject_class_id))
       end
     when "docente"
-      @subject_classes = SubjectClass.where(teacher_id: @teacher.id)
+      subject_classes = SubjectClass.where(teacher_id: @teacher.id)
     end
 
-    if @subject_classes.blank?
+    if subject_classes.blank?
       @forms = []
-      @errors << "Usuário não está associado a nenhuma turma"
+      errors << "Usuário não está associado a nenhuma turma"
     else
-      @forms = Form.where(subject_class_id: @subject_classes.pluck(:id), role: occupation)
-
-      @pending_forms = []
-      @answered_forms = []
+      @forms = Form.where(subject_class_id: subject_classes.pluck(:id), role: occupation)
 
       @forms.each do |form|
         @form_questions = FormQuestion.where(form_id: form.id)
-
-        case occupation
-        when "discente"
-          answers = StudentAnswer.where(form_question_id: @form_questions.pluck(:id))
-        when "docente"
-          answers = TeacherAnswer.where(form_question_id: @form_questions.pluck(:id))
-        end
-
+        form_question_ids = @form_questions.pluck(:id)
+        
+        answers = case occupation
+                  when "discente"
+                    StudentAnswer.where(form_question_id: form_question_ids)
+                  when "docente"
+                    TeacherAnswer.where(form_question_id: form_question_ids)
+                  end
+      
         if answers.any?
-          @answered_forms << form
+          answered_forms << form
         else
-          @pending_forms << form
+          pending_forms << form
         end
-
-        @forms = if @filter == "pending"
-            @pending_forms
-          else
-            @answered_forms
-          end
       end
+
+      @forms = if filter == "pending"
+                 pending_forms
+               else
+                 answered_forms
+               end
     end
   end
 
   def edit
     @form = Form.find_by_id(params[:id])
     @form_questions = FormQuestion.where(form_id: @form.id)
-
   end
 
   def update; end
@@ -66,15 +73,15 @@ class FormsController < ApplicationController
     @form = Form.find_by_id(params[:id])
     @form_questions = FormQuestion.where(form_id: @form.id)
 
-    @questions_and_answers = []
+    answer_model, identifier = if current_user.occupation == "discente"
+                                [StudentAnswer, { student_id: @student.id }]
+                              else
+                                [TeacherAnswer, { teacher_id: @teacher.id }]
+                              end
+
     @form_questions.each do |question|
-      if current_user.occupation == "discente"
-        answer = StudentAnswer.find_by({ student_id: @student.id, form_question_id: question.id })
-        @questions_and_answers << [question, answer]
-      else
-        answer = TeacherAnswer.find_by({ teacher_id: @teacher.id, form_question_id: question.id })
-        @questions_and_answers << [question, answer]
-      end
+      answer = answer_model.find_by(identifier.merge({ form_question_id: question.id }))
+      questions_and_answers << [question, answer]
     end
   end
 end
