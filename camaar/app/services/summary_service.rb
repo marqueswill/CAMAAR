cc850b025bf9885ee55cb41a2a3aef4724f6d291
Willyan Marques <willyanmarquesmelo@gmail.com>
2024-06-27 16:43:43 -0300

    ...

--- camaar/app/services/summary_service.rb
+++ camaar/app/services/summary_service.rb
@@ -1,28 +1,29 @@
 class SummaryService
-  def self.call(resumo, form_questions, form)
+  def self.call(form_questions, form)
+    resumo = {}
     form_questions.each do |question|
-      answers = if form.role == 'discente'
-                  StudentAnswer.where(form_question_id: question.id)
-                else
-                  TeacherAnswer.where(form_question_id: question.id)
-                end
+      answers = if form.role == "discente"
+          StudentAnswer.where(form_question_id: question.id)
+        else
+          TeacherAnswer.where(form_question_id: question.id)
+        end
 
       question_body = JSON.parse(question.body)
       answers.each do |answ|
-        answer_body = JSON.parse(answ.answers)['answers']
+        answer_body = JSON.parse(answ.answers)["answers"]
 
         case question.question_type
-        when 'text'
+        when "text"
           resumo[question.title] ||= []
           resumo[question.title] << answer_body
-        when 'multiple_choice'
+        when "multiple_choice"
           resumo[question.title] ||= {}
-          question_body['options'].each do |option|
-            resumo[question.title][option[1]] ||= 0 if option[1] != ''
+          question_body["options"].each do |option|
+            resumo[question.title][option[1]] ||= 0 if option[1] != ""
           end
 
           answer_body.each do |k, selected|
-            resumo[question.title][question_body['options'][k]] += 1 if selected
+            resumo[question.title][question_body["options"][k]] += 1 if selected
           end
         end
       end
--- camaar/lib/results.rb
+++ camaar/lib/results.rb
@@ -1,6 +1,6 @@
 class Results < AdminsController
-   # Método auxiliar que verifica se houve questões respondidas por professores ou alunos, retornando um booleano
-   def answers?(form_questions)
+  # Método auxiliar que verifica se houve questões respondidas por professores ou alunos, retornando um booleano
+  def answers?(form_questions)
     form_pluck = form_questions.pluck(:id)
     students_answers = StudentAnswer.where(form_question_id: form_pluck) if form_questions
     teacher_answers = TeacherAnswer.where(form_question_id: form_pluck) if form_questions
@@ -9,8 +9,7 @@ class Results < AdminsController
 
   # Método que gerencia as requisições para efetuar um resumo das respostas de todos os usuários acerca de um
   # formulário.
-  def generate_summary(form_questions,form)
-    resumo = {}
-    SummaryService.call(resumo, form_questions, form)
+  def generate_summary(form_questions, form)
+    SummaryService.call(form_questions, form)
   end
 end
