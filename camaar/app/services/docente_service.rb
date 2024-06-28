4cf79a4d8e5d7074c22b3e62b06a9ff39040b193
jpantonow <jpantonow@gmail.com>
2024-06-28 15:35:31 -0300

    correcting name error

--- camaar/app/services/docente_service.rb
+++ camaar/app/services/docente_service.rb
@@ -2,7 +2,7 @@ class DocenteService
     def initialize(user)
       @user = user
     end
-  
+
     def forms
       teacher = Teacher.find_by(user_id: @user.id)
       subject_classes = SubjectClass.find_by(teacher_id: teacher.id)
@@ -12,12 +12,12 @@ class DocenteService
         Form.where(subject_class_id: subject_classes.pluck(:id), role: 'docente')
       end
     end
-  
+
     def get_answers(form_questions)
       teacher = Teacher.find_by(user_id: @user.id)
       TeacherAnswer.where(form_question_id: form_questions.pluck(:id), teacher_id: teacher.id)
     end
-  
+
     def find_answer(question)
       teacher = Teacher.find_by(user_id: @user.id)
       TeacherAnswer.find_by(form_question_id: question.id, teacher_id: teacher.id)
