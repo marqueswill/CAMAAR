ccdfaabd8cabba673cf757fa50226cefe1798078
Marcos <marcosp.oliveir4@gmail.com>
2024-06-29 13:35:38 -0300

    sem smells e comentado

--- camaar/app/controllers/forms_controller.rb
+++ camaar/app/controllers/forms_controller.rb
@@ -1,35 +1,51 @@
+# A classe FormsController gerencia as funcionalidas referentes aos formulários.
+# Essa classe contém os métodos públicos responsáveis por mostrar e editar os formulários.
+# Também contém métodos privados que são responsáveis por realizar as partes mais lógicas do
+# código, como identificar o tipo de usuário, identificar as questões e as respostas de um formulário.
 class FormsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user_data
   before_action :initialize_data
   layout "user"
 
+  # Método para exibir a lista de formulários
   def index
-    @forms = filtered_forms
+    render :index, locals: { forms: filtered_forms }
   end
 
+  # Método para editar um formulário específico
   def edit
-    @form = Form.find(params[:id])
-    @form_questions = @form.form_question
+    form = find_form
+    render :edit, locals: { form: form, form_questions: form.form_question }
   end
 
+  # Método para atualizar um formulário (ainda não implementado)
   def update; end
 
+  # Método para exibir um formulário específico
   def show
-    @form = Form.find(params[:id])
-    @form_questions = @form.form_question
-    @questions_and_answers = set_questions_and_answers
+    form = find_form
+    form_questions = form.form_question
+    render :show, locals: { 
+      form: form, 
+      form_questions: form_questions,
+      questions_and_answers: set_questions_and_answers(form_questions)
+    }
   end
 
   private
 
+  # Atributos para evitar variáveis de instância
   attr_reader :user_service, :filter
 
+  # Inicializa os dados do usuário atual
   def initialize_data
     @user_service = user_service_for_current_user
     @filter = params[:filter] || "pending"
   end
 
+  # Retorna o tipo de usuário ("teacher" ou "student") para o método initalize_data realizar a
+  # inicialização corretamente
   def user_service_for_current_user
     if current_user.discente?
       DiscenteService.new(current_user)
@@ -40,6 +56,7 @@ class FormsController < ApplicationController
     end
   end
 
+  # Filtra os formulários entre "pendentes" e "respondidos" para a redenrização correta na view correspondente
   def filtered_forms
     forms = user_service.forms
     return [] if forms.empty?
@@ -48,28 +65,21 @@ class FormsController < ApplicationController
     filter == "pending" ? categorized_forms[:pending] : categorized_forms[:answered]
   end
 
+  # Categoriza os formulários em pendentes e respondidos para o método filtered_forms
   def categorize_forms(forms)
-    forms.each_with_object(pending: [], answered: []) do |form, result|
-      if form_answered?(form)
-        result[:answered] << form
-      else
-        result[:pending] << form
-      end
-    end
+    FormCategorizerService.categorize(forms, user_service)
   end
   
-  def set_questions_and_answers
-    @form_questions.map do |question|
+  # Define as questões e respostas para um formulário específico
+  def set_questions_and_answers(form_questions)
+    form_questions.map do |question|
       answer = user_service.find_answer(question)
       [question, answer]
     end
   end
 
-  def form_answered?(form)
-    answered = form.form_question.all? do |question|
-      answer = user_service.find_answer(question)
-      answer.present?
-    end
-    answered
+  # Encontra um formulário pelo ID
+  def find_form
+    Form.find(params[:id])
   end
 end
