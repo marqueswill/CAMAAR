95627eda76cc33f3c9cef84f91e191583dda3ccd
jpantonow <jpantonow@gmail.com>
2024-07-02 16:15:02 -0300

    refactoring services and libs

--- camaar/lib/answers.rb
+++ camaar/lib/answers.rb
@@ -3,6 +3,5 @@
 class Answers < AnswersController
   def submit_answers(answers_params, current_user, form)
     AnswerCreationService.new(current_user, answers_params, form).create_answers
-    ['/users/forms']
   end
 end
