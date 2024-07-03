0138a41f878300a7414f48f557f481ef9ca7420a
artdelpi <artdelpi@gmail.com>
2024-07-03 14:35:40 -0300

    breve correção

--- camaar/app/controllers/classes_controller.rb
+++ camaar/app/controllers/classes_controller.rb
@@ -2,7 +2,6 @@
 # às classes que o administrador tem acesso e é vinculada a uma model.
 class ClassesController < ApplicationController
   # Método para exibir a lista de classes
-  # Associa a variável de instância @classes que será usada na view index.html.erb
   def index; end
 
   # Método para importar dados das classes
